a77bd3a5b6ff3a13c5f28f548ae226d24e696296
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2017-10-29 13:19:39 -0700

    Remove non-expressive comments

--- src/main/java/com/developersdelicias/codesmells/comments/bad/ExplainingBadCode.java
+++ src/main/java/com/developersdelicias/codesmells/comments/bad/ExplainingBadCode.java
@@ -2,16 +2,13 @@ package com.developersdelicias.codesmells.comments.bad;
 
 public class ExplainingBadCode {
 
-    // The number max of process to handle
-    private final int NOP = 15;
-    // Total of process in memory
-    private static int totProc = 0;
+    private final int MAX_NUMBER_OF_PROCESS_TO_HANDLE = 15;
+    private static int totalOfProcessInMemory = 0;
 
     public void handleProcess() {
-        // Current Process Identifier
-        long currPrId = Thread.currentThread().getId();
+        long currentProcessId = Thread.currentThread().getId();
 
-        if (totProc > NOP) {
+        if (totalOfProcessInMemory > MAX_NUMBER_OF_PROCESS_TO_HANDLE) {
             throw new RuntimeException("No more process can be handle");
         }
     }
